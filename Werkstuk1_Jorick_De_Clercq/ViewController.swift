4e8848996293288eb9a17fed77d31d1629605dd8
DE CLERCQ Jorick (s) <jorick.de.clercq@dt-c205-25.local>
2018-05-03 15:17:05 +0200

    Initial Commit

---
+++ Werkstuk1_Jorick_De_Clercq/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werkstuk1_Jorick_De_Clercq
+//
+//  Created by DE CLERCQ Jorick (s) on 03/05/2018.
+//  Copyright © 2018 DE CLERCQ Jorick (s). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Werkstuk1_Jorick_De_ClercqTests/Werkstuk1_Jorick_De_ClercqTests.swift
@@ -0,0 +1,36 @@
+//
+//  Werkstuk1_Jorick_De_ClercqTests.swift
+//  Werkstuk1_Jorick_De_ClercqTests
+//
+//  Created by DE CLERCQ Jorick (s) on 03/05/2018.
+//  Copyright © 2018 DE CLERCQ Jorick (s). All rights reserved.
+//
+
+import XCTest
+@testable import Werkstuk1_Jorick_De_Clercq
+
+class Werkstuk1_Jorick_De_ClercqTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
